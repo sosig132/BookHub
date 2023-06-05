9ac7320800ec27ed065e92bc1d380050106eb326
sosig132 <sosig132@gmail.com>
2023-06-05 05:07:12 +0300

    Finished adding books and showing them

--- Front_End/src/app/pages/home/home.component.ts
+++ Front_End/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { Book } from '../../data/interfaces/book';
 import { BookService } from '../../services/book/book.service';
@@ -12,19 +12,29 @@ import { Observable } from 'rxjs';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   books: Book[];
   displayedColumns: string[] = ['image', 'title', 'author', 'category'];
-  constructor(private changeDetectorRef: ChangeDetectorRef, private readonly bookService: BookService) { }
-  len: number;
-  ngOnInit(): void {
+  constructor(private changeDetectorRef: ChangeDetectorRef, private readonly bookService: BookService) {
     this.bookService.getAllBooks().subscribe(
       books => {
         this.books = books as Book[];
-        this.len=books.length;
+        this.len = books.length;
+        this.dataSource = new MatTableDataSource<Book>(this.books);
+        this.dataSource.paginator = this.paginator;
+
       }
     );
+  }
+  len: number;
+  ngOnInit(): void {
+    console.log(this.dataSource)
+  }
+
+  dataSource: MatTableDataSource<Book>;
+
+  ngAfterViewInit() {
   }
 
   
